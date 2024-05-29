2b02b677805db5041113b0b0909d0baccc44c098
Ulrich <nzeuleuu@gmail.com>
2024-05-29 15:32:05 +0100

    nouvelle commande client fournisseurs okay.

--- src/app/pages/page-cmd-clt-frs/page-cmd-clt-frs.component.ts
+++ src/app/pages/page-cmd-clt-frs/page-cmd-clt-frs.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-cmd-clt-frs',
   templateUrl: './page-cmd-clt-frs.component.html',
   styleUrl: './page-cmd-clt-frs.component.css'
 })
-export class PageCmdCltFrsComponent {
+export class PageCmdCltFrsComponent implements OnInit {
+
+  constructor( private router: Router){}
+
+  ngOnInit(): void {
+  }
+
+  nouvelleCommande():void {
+    this.router.navigate(['nouvellecommandesclt']);
+  }
 
 }
