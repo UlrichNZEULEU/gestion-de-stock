fd877f44bbd7adc8cb57eb29e16c8e30578a7da3
Ulrich <nzeuleuu@gmail.com>
2024-05-29 23:17:01 +0100

    interface changer mot de passe okay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -102,7 +102,7 @@ const routes: Routes = [
         component: PageProfilComponent
       },
       {
-        path: 'changerotdepasse',
+        path: 'changermotdepasse',
         component: ChangerMotDePasseComponent
       }
     ]
--- src/app/pages/profil/page-profil/page-profil.component.ts
+++ src/app/pages/profil/page-profil/page-profil.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-profil',
   templateUrl: './page-profil.component.html',
   styleUrl: './page-profil.component.css'
 })
-export class PageProfilComponent {
+export class PageProfilComponent implements OnInit {
+
+  constructor( private router: Router){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  modifierMotDePasse():void {
+    this.router.navigate(['changermotdepasse']);
+  }
 
 }
